c17f1d30ba415fc9c8e8d60ee8c54858356339db
Raphael <raphaelrsalmeida@gmail.com>
2022-07-06 15:29:55 -0300

    criando a pasta do controller cadastro

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CadastroController } from './cadastro/cadastro.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CadastroController],
   providers: [AppService],
 })
 export class AppModule {}
